7a0112bdcdd9082b049a0c661a2096e27eb63e19
Tom Haug <tomhaug75@gmail.com>
2022-11-26 13:51:05 -0500

    assert > 0 tests

--- src/shared/base_test.py
+++ src/shared/base_test.py
@@ -11,18 +11,24 @@ class BaseTest(ABC):
     def get_controller_b(self) -> Controller:
         ...
 
-    def test_part01(self):
+    def test_part_a(self):
         controller = self.get_controller_a()
         tests = controller.sample_files()
+        
+        assert len(tests) > 0
+        
         for (file, expected_result) in tests:
             solver = controller.new_solver()
             solver.initialize(file)
             result = solver.solve()
             assert result == expected_result
 
-    def test_part02(self):
+    def test_part_b(self):
         controller = self.get_controller_b()
         tests = controller.sample_files()
+        
+        assert len(tests) > 0
+        
         for (file, expected_result) in tests:
             solver = controller.new_solver()
             solver.initialize(file)
